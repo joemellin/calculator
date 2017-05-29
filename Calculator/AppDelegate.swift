aa36cc96d283ba829154811c250bd4186adcc047
buddybuild <help@buddybuild.com>
2017-05-28 23:21:07 -0700

    Adding buddybuild SDK
    - Add BuddyBuildSDK.framework
    - Initialize in App Delegate

--- Calculator/AppDelegate.swift
+++ Calculator/AppDelegate.swift
@@ -19,6 +19,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        BuddyBuildSDK.setup()
+        
         // Override point for customization after application launch.
         
         MSMobileCenter.start("c625ba0f-ac7b-448c-b05a-e9725b3e6491", withServices: [MSAnalytics.self, MSCrashes.self])
